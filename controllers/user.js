5a0e67a3b8151aa1db475507b99d9305c97a552b
Denis Zykov <denis.zykov.1993@gmail.com>
2023-08-13 11:59:41 +0300

    fixed: delete sameSite

--- controllers/user.js
+++ controllers/user.js
@@ -81,7 +81,7 @@ module.exports.login = (req, res, next) => {
           NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
           { expiresIn: '7d' },
         );
-        res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true, sameSite: true });
+        res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true });
         return res.send({
           _id: user._id,
           name: user.name,
