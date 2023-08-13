9e7982762442534a2f0672d319f03fb7b6615bb8
Denis Zykov <denis.zykov.1993@gmail.com>
2023-08-13 10:28:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.header('Origin'));
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 app.use(cors({
   origin: [
