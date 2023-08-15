1332192f35c84f279e73071fbcfda2a568b219c6
Denis-Deonis <73986601+Denis-Deonis@users.noreply.github.com>
2023-08-15 08:35:19 +0300

    add default data

--- models/user.js
+++ models/user.js
@@ -6,16 +6,18 @@ const userSchema = new mongoose.Schema(
     name: {
       type: String,
       required: false,
+      default: 'Жак-Ив Кусто',
       minlength: [2, 'Минимальная длина поля 2 символа'],
       maxlength: [30, 'Максимальная длина поля 30 символов'],
     },
     email: {
       type: String,
       required: true,
+      default: 'some@email.com',
       unique: true,
       validate: {
-        validator: (email) => validator.isEmail(email),
-        message: 'Введен некорректный адрес электронной почты',
+        validator: (v) => validator.isEmail(v),
+        message: () => 'Введен некорректный email адрес',
       },
     },
     password: {
