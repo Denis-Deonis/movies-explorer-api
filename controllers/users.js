557ed3b1fe3d96ed078bf2ac36a51d28ee35095e
Denis Zykov <denis.zykov.1993@gmail.com>
2023-08-22 06:21:29 +0300

    res.send({token})

--- controllers/users.js
+++ controllers/users.js
@@ -39,12 +39,7 @@ module.exports.login = (req, res, next) => {
           NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
           { expiresIn: '7d' }
         )
-       return res.send({
-               _id: user._id,
-              name: user.name,
-              email: user.email,
-              jwt: token
-       });
+       return res.send({token});
       })
     })
     .catch(next)
